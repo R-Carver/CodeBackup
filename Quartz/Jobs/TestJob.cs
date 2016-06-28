7b25706e4afc18ce32631efb8d836b241a78e23a
R-Carver <davidmakowski00@gmail.com>
2016-06-28 02:10:12 +0200

    Add files via upload

---
+++ Quartz/Jobs/TestJob.cs
@@ -0,0 +1,52 @@
+﻿using Quartz;
+using Quartz.Impl.Triggers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Quartz.Jobs
+{
+    public class TestJob : IJob 
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            System.Diagnostics.Debug.WriteLine("Aufgabe wird geloescht ");
+
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+            int taskKey = dataMap.GetInt("TaskId");
+
+            MyDbContext db = new MyDbContext();
+
+            ContractTask task = db.ContractTasks.Find(taskKey);
+            db.ContractTasks.Remove(task);
+            db.SaveChanges();
+
+            System.Diagnostics.Debug.WriteLine("Aufgabe wurde geloescht ");
+        }
+
+    }
+
+    public class RemoveTaskWhenDone : IJob
+    {
+        public void Execute(IJobExecutionContext context)
+        {
+            System.Diagnostics.Debug.WriteLine("Aufgabe wird geloescht ");
+
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+
+            int taskKey = dataMap.GetInt("TaskId");
+
+            MyDbContext db = new MyDbContext();
+
+            ContractTask task = db.ContractTasks.Find(taskKey);
+            db.ContractTasks.Remove(task);
+            db.SaveChanges();
+
+            System.Diagnostics.Debug.WriteLine("Aufgabe wurde geloescht ");
+        }
+
+    }
+}
