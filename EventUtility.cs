b8301376df7065db94f92b8018ce3e298d9cbc3e
R-Carver <davidmakowski00@gmail.com>
2016-06-30 20:53:14 +0200

    Add files via upload

--- EventUtility.cs
+++ EventUtility.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity.EntityFramework;
 using Quartz;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using WebApplication1.Models;
@@ -35,10 +36,12 @@ namespace WebApplication1.Utilities.EventSystem
                 tempTask.IsDone = false;
 
                 tempTask.DateCreated = DateTime.Now;
-                tempTask.Expiring = DateTime.Now.AddSeconds(30);
+                tempTask.Expiring = DateTime.Now.AddSeconds(40);
 
                 db.ContractTasks.Add(tempTask);
                 db.SaveChanges();
+
+                tempTask.TriggerTaskCreatedEvent();
 
                 System.Diagnostics.Debug.WriteLine("Aufgabe erstellt");
 
@@ -49,23 +52,52 @@ namespace WebApplication1.Utilities.EventSystem
 
         public static void OnDispatcherSet(object source, EventArgs e)
         {
-            //Not implemented yet
-            
+            //Not implemented yet - Mark Task as Done 
+            MyDbContext db = new MyDbContext();
+
+            var contract = (Contract)source;
+
+            var task = db.ContractTasks.Where(t => t.Contract.Id == contract.Id).SingleOrDefault();
+            task.IsDone = true;
+
+            db.Entry(task).State = EntityState.Modified;
+            db.SaveChanges();
+
+            task.TriggerTaskDoneEvent();
+
+            System.Diagnostics.Debug.WriteLine("Is Done checken ");
+
         }
 
         public static void OnTaskDone(object source, EventArgs e)
         {
+            System.Diagnostics.Debug.WriteLine("OnTaskDone happened");
             //when task is marked as done schedule the remove process some time after
             ContractTask tempTask = (ContractTask)source;
 
             //Call the Scheduler Singleton and Instantiate the job
             IScheduler scheduler = Scheduler.Instance();
-            IJobDetail job = JobBuilder.Create<RemoveTaskWhenDone>().Build();
+            IJobDetail job = JobBuilder.Create<RemoveTask>().Build();
 
             //Pass the Id of the task to the job for deleting it
             job.JobDataMap["TaskId"] = tempTask.Id;
 
             scheduler.ScheduleJob(job, Triggers.GetTriggerWithSecondOffset(30));
+        }
+
+        public static void OnTaskCreated(object source, EventArgs e)
+        {
+            //make sure that task is removed on expiration date
+            ContractTask tempTask = (ContractTask)source;
+
+            //Call the Scheduler Singleton and Instantiate the job
+            IScheduler scheduler = Scheduler.Instance();
+            IJobDetail job = JobBuilder.Create<RemoveTask>().Build();
+
+            //Pass the Id of the task to the job for deleting it
+            job.JobDataMap["TaskId"] = tempTask.Id;
+
+            scheduler.ScheduleJob(job, Triggers.GetTriggerAtDate((DateTime)tempTask.Expiring));
         }
         //DAVID TaskTest *********************************************************************************ENDE
     }
