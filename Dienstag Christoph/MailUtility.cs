dd9033664d802393ed1117d34fc9a8f2d788c56e
R-Carver <davidmakowski00@gmail.com>
2016-07-12 17:52:40 +0200

    Add files via upload

---
+++ Dienstag Christoph/MailUtility.cs
@@ -0,0 +1,54 @@
+﻿using Postal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Utilities.EventSystem
+{
+    public class MailUtility
+    {
+        private static string applicationEmail = "soproteam07@gmx.de";
+
+        public static void OnDispatcherSet(object source, EventArgs e)
+        {
+            MyDbContext db = new MyDbContext();
+
+            Contract tempContract = (Contract)source;
+
+            //The dispatcher has to be loaded here because the nav property is not set at this point yet
+            ContractUser dispatcher = db.Users.Find(tempContract.DispatcherId);
+
+            //Notify Dispatcher
+            dynamic email = new Email("DispatcherMail");
+            email.to = dispatcher.Email;
+
+            email.from = applicationEmail;
+            email.contractId = tempContract.Id;
+
+            email.Send();
+            System.Diagnostics.Debug.WriteLine("Email wurde abgeschickt");
+        }
+
+        public static void NotifyTaskOwner(ContractTask task)
+        {
+            dynamic email = new Email("NotificationMail");
+            email.to = task.User.Email;
+            email.from = applicationEmail;
+            
+            email.Send();
+        }
+
+        public static void SendReportMail(string attachmentPath, ContractUser user)
+        {
+            dynamic email = new Email("ReportMail");
+            email.to = user.Email;
+            email.from = applicationEmail;
+
+            email.Attach(new Attachment(attachmentPath));
+            email.Send();
+        }
+    }
+}
